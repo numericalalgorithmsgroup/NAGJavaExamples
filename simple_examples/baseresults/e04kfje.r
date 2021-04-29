9be94fbda6a5dc4a68b68d9a6d8645698deb3f92
Christos Efstathiou <christos.efstathiou@nag.co.uk>
2021-04-29 11:41:22 +0100

    Simple Examples Synced

---
+++ simple_examples/baseresults/e04kfje.r
@@ -0,0 +1,55 @@
+ E04KFJ Example Program Results
+
+ E04KF, First order method for bound-constrained problems                       
+ Begin of Options                                                               
+     Print File                    =                   6     * d                
+     Print Level                   =                   1     * U                
+     Print Options                 =                 Yes     * d                
+     Print Solution                =                 All     * U                
+     Monitoring File               =                   9     * U                
+     Monitoring Level              =                   3     * U                
+     Foas Monitor Frequency        =                   0     * d                
+     Foas Print Frequency          =                   5     * U                
+ 
+     Infinite Bound Size           =         1.00000E+20     * d                
+     Task                          =            Minimize     * d                
+     Stats Time                    =                  No     * d                
+     Time Limit                    =         1.00000E+06     * d                
+     Verify Derivatives            =                  No     * d                
+ 
+     Foas Estimate Derivatives     =                  No     * d                
+     Foas Finite Diff Interval     =         1.05367E-08     * d                
+     Foas Iteration Limit          =            10000000     * d                
+     Foas Memory                   =                  11     * d                
+     Foas Progress Tolerance       =         1.08158E-12     * d                
+     Foas Rel Stop Tolerance       =         1.08158E-12     * d                
+     Foas Restart Factor           =         6.00000E+00     * d                
+     Foas Slow Tolerance           =         1.01316E-02     * d                
+     Foas Stop Tolerance           =         1.00000E-06     * d                
+     Foas Tolerance Norm           =            Infinity     * d                
+ End of Options                                                                 
+                                                                                
+                                                                                                    
+ Status: converged, an optimal solution was found                                                   
+ Value of the objective             4.00000E-02                                                     
+ Norm of inactive gradient          0.00000E+00                                                     
+ Norm of projected direction        0.00000E+00                                                     
+                                                                                
+ Primal variables:                                                              
+   idx   Lower bound       Value       Upper bound                              
+     1  -1.00000E+00    8.00000E-01    8.00000E-01                              
+     2  -2.00000E+00    6.40000E-01    2.00000E+00                              
+                                                                                
+ Box bounds dual variables:                                                     
+   idx   Lower bound       Value       Upper bound       Value                  
+     1  -1.00000E+00    0.00000E+00    8.00000E-01    4.00000E-01               
+     2  -2.00000E+00    0.00000E+00    2.00000E+00    0.00000E+00               
+
+
+ Solution found:
+  Objective function value at solution:   4.0E-02
+  Gradient at solution:                  -4.0E-01   0.0E+00
+
+  Estimated Lagrange multipliers: blx     0.0E+00   0.0E+00
+  Estimated Lagrange multipliers: bux     4.0E-01   0.0E+00
+
