f625b3f043c5c14a88d7ecbc04374acf75d63c82
Christos Efstathiou <christos.efstathiou@nag.co.uk>
2021-05-05 14:48:37 +0100

    Simple Examples Synced with branch MK27.2

--- simple_examples/baseresults/a00aaje.r
+++ simple_examples/baseresults/a00aaje.r
@@ -5,6 +5,6 @@
  Implementation title: Linux, 64-bit, Intel C/C++ (32-bit integers)
             Precision: double precision
          Product Code: CLL6I262CL
-                 Mark: 27.1.3 (self-contained)
+                 Mark: 27.2.1 (self-contained)
 
  *** End of NAG Library implementation details ***
--- simple_examples/baseresults/outputexample.r
+++ simple_examples/baseresults/outputexample.r
@@ -3,7 +3,7 @@
  Implementation title: Linux, 64-bit, Intel C/C++ or Intel Fortran                                                                  
             Precision: double precision                                                                                             
          Product Code: NLL6I27DBL                                                                                                   
-                 Mark: 27.1.3 (self-contained)                                                                                      
+                 Mark: 27.2.1 (self-contained)                                                                                      
                                                                                                                                     
   This is a 64-bit library using 32-bit integers.                                                                                   
                                                                                                                                     
